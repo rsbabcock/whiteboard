541666dff96b3ad6a026d6d3de2aca3fc99e9756
rsbabcock <rachael.s.babcock@gmail.com>
2018-08-01 11:26:28 -0500

    added fizzbuzz

--- whiteboard.py
+++ whiteboard.py
@@ -57,3 +57,19 @@ def fibonacci():
     return fib[1:]
 
 print(fibonacci())    
+
+# Implement FizzBuzz
+def FizzBuzz():
+    for fizzbuzz in range(20):
+        if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+            print("fizzbuzz")
+            continue
+        elif fizzbuzz % 3 == 0:
+            print("fizz")
+            continue
+        elif fizzbuzz % 5 == 0:
+            print("buzz")
+            continue
+    print(fizzbuzz)
+
+print(FizzBuzz())    
